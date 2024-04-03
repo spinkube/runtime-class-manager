9e6b013301a81a7c151ef67f8eee6995520dcbe8
Tom Gehrke <phyrog@gmail.com>
2024-04-03 20:51:11 +0200

    implement some basic k8s distro detection

--- cmd/node-installer/uninstall.go
+++ cmd/node-installer/uninstall.go
@@ -26,6 +26,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/spinkube/runtime-class-manager/internal/containerd"
+	"github.com/spinkube/runtime-class-manager/internal/preset"
 	"github.com/spinkube/runtime-class-manager/internal/shim"
 )
 
@@ -36,13 +37,18 @@ var uninstallCmd = &cobra.Command{
 	Run: func(_ *cobra.Command, _ []string) {
 		rootFs := afero.NewOsFs()
 		hostFs := afero.NewBasePathFs(rootFs, config.Host.RootPath)
-		restarter := containerd.NewRestarter()
 
-		if err := RunUninstall(config, rootFs, hostFs, restarter); err != nil {
-			slog.Error("failed to uninstall shim", "error", err)
+		distro, err := DetectDistro(config, hostFs)
+		if err != nil {
+			slog.Error("failed to detect containerd config", "error", err)
+			os.Exit(1)
+		}
 
-			// Exiting with 0 to prevent Kubernetes Jobs from running repetitively
-			os.Exit(0)
+		config.Runtime.ConfigPath = distro.ConfigPath
+
+		if err := RunUninstall(config, rootFs, hostFs, distro.Restarter(preset.Env{ConfigPath: distro.ConfigPath, HostFs: hostFs})); err != nil {
+			slog.Error("failed to uninstall", "error", err)
+			os.Exit(1)
 		}
 	},
 }
